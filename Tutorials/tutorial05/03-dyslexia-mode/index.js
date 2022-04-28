38c1e207e7316bc160084e072a536c3773d86387
marucc07 <clairecai19@gmail.com>
2022-04-27 19:16:31 -0500

    Tutorial 05

---
+++ Tutorials/tutorial05/03-dyslexia-mode/index.js
@@ -0,0 +1,14 @@
+/* 
+  See Smashing Magazine Tutorial:
+  https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
+*/
+
+const toggle = document.querySelector("#dyslexia-toggle");
+
+const toggleEventHandler = (ev) => {
+  /* alert("hh"); */
+  /* console.log("test");*/
+  document.body.classList.toggle("dyslexia-mode");
+};
+
+toggle.addEventListener("click", toggleEventHandler); 
