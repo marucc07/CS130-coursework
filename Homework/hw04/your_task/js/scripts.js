ce5bc0be1562e669c160c25c42c5ff894da9547b
marucc07 <clairecai19@gmail.com>
2022-05-25 15:04:47 -0500

    HW4

--- Homework/hw04/your_task/js/scripts.js
+++ Homework/hw04/your_task/js/scripts.js
@@ -17,18 +17,69 @@ const search = (ev) => {
 }
 
 const getTracks = (term) => {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
-};
+// console.log(`
+//        get tracks from spotify based on the search term
+//        "${term}" and load the first five tracks into the #tracks section 
+//        of the DOM...`);
+const elem = document.querySelector("#tracks");
+elem.innerHTML = "";
+
+fetch(baseURL + "?type=track&limit=5&q=" + term)
+    .then(response => response.json())
+    .then(tracks => {
+        console.log(tracks);
+        if (tracks.length === 0) {
+            document.querySelector("#tracks").innerHTML += `
+            <p> no songs available </p>`;
+        }
+        for (const track of tracks) {
+            document.querySelector("#tracks").innerHTML += `
+            <button class="track-item preview" data-preview-track="${track.id}" onclick="handleTrackClick(event);">
+                <img alt="Image of ${track.name}" src="${track.album.image_url}">
+                <i class="fas play-track fa-play" aria-hidden="true"></i>
+                <div class="label">
+                    <h2>${track.name}</h2>
+                    <p>${track.artist.name}</p>
+                </div>
+            </button>`;
+        }
+    });
+}
+
 
 const getAlbums = (term) => {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
-};
+    // console.log(`
+//        get albums from spotify based on the search term
+//        "${term}" and load albums
+//        of the DOM...`);
+const elem = document.querySelector("#albums");
+elem.innerHTML = "";
+
+fetch(baseURL + "?type=album&q=" + term)
+    .then(response => response.json())
+    .then(albums => {
+        console.log(albums);
+        if (albums.length === 0) {
+            document.querySelector("#albums").innerHTML += `
+            <p> no albums available </p>`;
+        }
+        for (const album of albums) {
+            document.querySelector("#albums").innerHTML += `
+            <section class="album-card" id="${album.id}">
+                <div>
+                    <img alt="Image of ${album.name}" src="${album.image_url}">
+                    <h2>${album.name}</h2>
+                    <div class="footer">
+                        <a href=${album.spotify_url} target="_blank">
+                            view on spotify
+                        </a>
+                    </div>
+                </div>
+            </section>`;
+        }
+    });
+}
+
 
 const getArtist = (term) => {
 // console.log(`
@@ -69,10 +120,18 @@ const getArtistHTML = (data) => {
             </section>`;
 };
 
+
 const handleTrackClick = (ev) => {
-    const previewUrl = ev.currentTarget.getAttribute('data-preview-track');
-    console.log(previewUrl);
-}
+        const currentTrackElem = document.querySelector("#current-track");
+        currentTrackElem.innerHTML = ev.currentTarget.innerHTML;
+        const preview_url = ev.currentTarget.dataset.previewTrack;
+        console.log(preview_url);
+        if (audioPlayer.getAudioFile() !== preview_url) {
+            audioPlayer.setAudioFile(preview_url);
+        }
+        audioPlayer.play();
+    };
+
 
 document.querySelector('#search').onkeyup = (ev) => {
     // Number 13 is the "Enter" key on the keyboard
